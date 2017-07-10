aa5e1b3466d348c63abbe581cb3f2f4fd81cd47b
d-kang <david.kang714@gmail.com>
2017-07-10 12:07:06 -0700

    ForagerBee is Subclass of Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  Bee.call(this); //inherits properties
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+};
+// Inherits methods from superclass
+ForagerBee.prototype = Object.create(Bee.prototype);
+// .prototype.constructor property points back to the constructor itself
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
 };
 
+
+var foragerBee = new ForagerBee();
+console.log({foragerBee})
