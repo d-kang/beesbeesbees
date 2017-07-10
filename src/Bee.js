3b11a11ce2670a268fcf6928e73e55c1177d5eb1
d-kang <david.kang714@gmail.com>
2017-07-10 11:52:39 -0700

    Bee Is Subclass of Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  Grub.call(this)
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype)
+Bee.prototype.constructor = Bee;
+
+var bee = new Bee();
+console.log({bee})
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/create
